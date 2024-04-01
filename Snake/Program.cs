bafe9e9d3c39f029bf5157eab36ebaec0d1f6ba7
Zmeelov7598 <Zmeelov7598@yandex.ru>
2024-04-01 20:20:59 +0600

    Finish Lesson8

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,14 +10,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize(80, 25);
 
-            p1.Draw();
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
-            Point p2 = new Point(4, 5, '#');
-
-            p2.Draw();
-
+            Point p = new Point(4, 5, '*');
+            p.Draw();
             Console.ReadLine();
         }
     }
---
+++ Snake/VerticalLine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine : Figure
+    {
+        public VerticalLine(int yUp, int yDowm, int x, char sym)
+        {
+            pList = new List<Point>();
+            for(int y = yUp; y <= yDowm; y++) 
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+    }
+}
